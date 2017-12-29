d67c8a9b6ebfab61604035eef39bfb5b7f84ebf4
Tsagan Garyaeva <tsagan81@gmail.com>
2017-12-29 15:52:52 -0500

    VB lab

--- Lab10LookUp/Form1.Designer.vb
+++ Lab10LookUp/Form1.Designer.vb
@@ -22,9 +22,140 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lstBox = New System.Windows.Forms.ListBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSearch
+        '
+        Me.btnSearch.BackColor = System.Drawing.Color.DarkBlue
+        Me.btnSearch.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnSearch.Location = New System.Drawing.Point(204, 262)
+        Me.btnSearch.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(141, 47)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = False
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.Color.LightYellow
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.Maroon
+        Me.lblResult.Location = New System.Drawing.Point(204, 198)
+        Me.lblResult.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(291, 28)
+        Me.lblResult.TabIndex = 6
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.DarkBlue
+        Me.Label2.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(50, 198)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 28)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Result:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnclose
+        '
+        Me.btnclose.BackColor = System.Drawing.Color.DarkBlue
+        Me.btnclose.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnclose.Location = New System.Drawing.Point(366, 262)
+        Me.btnclose.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(129, 47)
+        Me.btnclose.TabIndex = 9
+        Me.btnclose.Text = "Close"
+        Me.btnclose.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.DarkBlue
+        Me.Label3.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(50, 21)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 28)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Enter:"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstBox
+        '
+        Me.lstBox.FormattingEnabled = True
+        Me.lstBox.ItemHeight = 18
+        Me.lstBox.Location = New System.Drawing.Point(50, 80)
+        Me.lstBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.lstBox.Name = "lstBox"
+        Me.lstBox.Size = New System.Drawing.Size(444, 94)
+        Me.lstBox.TabIndex = 10
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(204, 21)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(289, 26)
+        Me.txtSearch.TabIndex = 11
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.DarkBlue
+        Me.btnClear.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnClear.Location = New System.Drawing.Point(50, 262)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(134, 47)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.BackColor = System.Drawing.Color.Navy
+        Me.ClientSize = New System.Drawing.Size(542, 353)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.lstBox)
+        Me.Controls.Add(Me.btnclose)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Famous Programmers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnclose As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lstBox As ListBox
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnClear As Button
 End Class
--- Lab10LookUp/Form1.vb
+++ Lab10LookUp/Form1.vb
@@ -1,3 +1,43 @@
-﻿Public Class Form1
+﻿
 
+
+'Tsagan Garyaeva
+'Comp-185
+'Lab 10 LookUp
+Public Class Form1
+
+
+    Dim strProgrammers() As String = {"Linus Torvalds", "James Gosling", "Dennis Ritchie", "Tim Berners-Lee", "Ken Thompson", "Donald Knuth", "Bill Gates"}
+    Dim strPhones() As String = {"732-234-7676", "989-000-0998", "765-989-0986", "732-234-7676", "489-345-0998", "765-123-0986", "767-987-9876"}
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Dim strName As String = CStr(txtSearch.Text)
+        Dim intIndex As Integer
+        Dim strInfo As String
+        lstBox.Items.Clear()
+        strName = strName.ToLower()
+
+        For intIndex = 0 To strProgrammers.Length - 1
+            strInfo = strProgrammers(intIndex)
+            strInfo = strInfo.ToLower()
+            If strInfo.IndexOf(strName) <> -1 Then
+                lstBox.Items.Add(strProgrammers(intIndex) & "  " & strPhones(intIndex))
+
+            End If
+        Next
+        If lstBox.Items.Count = 0 Then
+            lblResult.Text = "No entries found"
+        End If
+
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtSearch.Clear()
+        lblResult.Text = " "
+        lstBox.Items.Clear()
+    End Sub
 End Class
